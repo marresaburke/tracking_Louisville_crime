f26797b7c70449467428b90dbe71c3f8b8e5bf60
marresaburke <marresa.burke@gmail.com>
2024-05-21 22:45:29 -0400

    Highest Charge per Age
    Trying to explore the data and figure out the code to find the highest charge per age.

--- working_data.py
+++ working_data.py
@@ -7,4 +7,16 @@ teen_data.columns
 
 date = teen_data['Date']
 date.describe()
-date.value_counts()
+date.value_counts().unique()
+
+age = teen_data.Age
+age.value_counts()
+
+charges = teen_data.Charges
+charges.value_counts()
+
+
+Age_v_charges = teen_data.groupby('Age')
+teen_data.columns
+
+
