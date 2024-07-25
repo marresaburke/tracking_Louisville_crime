21a10143005f6f3efb8201556b9c774e6465671a
marresaburke <marresa.burke@gmail.com>
2024-07-25 16:54:38 -0400

    update files figuring out glitch.

--- working_data.py
+++ working_data.py
@@ -3,23 +3,25 @@ import pandas as pd
 teen_data = pd.read_csv('LMPD_CleanedData_YouthCrimeFull_2023-2024.csv')
 teen_data
 
-teen_data.columns
-
-date = teen_data['Date']
-date.describe()
-date.value_counts().unique()
-
-age = teen_data.Age
-age.value_counts()
-
-charges = teen_data.Charges
-charges.value_counts()
 
 
+#teen_data.columns
 
-age_char_nums = Age_v_charges.value_counts()
+#date = teen_data['Date']
+#date.describe()
+#date.value_counts().unique()
 
-gun_count_data = teen_data.Charges.map(lambda Char: "GUN" in Char).sum()
+#age = teen_data.Age
+#age.value_counts()
+
+#charges = teen_data.Charges
+#charges.value_counts()
+
+
+
+#age_char_nums = Age_v_charges.value_counts()
+
+#gun_count_data = teen_data.Charges.map(lambda Char: "GUN" in Char).sum()
 
 #num_of_guns = pd.Series([gun_count_data],index = ["gun count"])
 
