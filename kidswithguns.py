72f714ed184a6e401640b23e077ba3d92a809f99
marresaburke <marresa.burke@gmail.com>
2024-07-26 23:46:49 -0400

    exploring data with guns and looking for answers and solutions. trying to find the NaN values and replace.

--- kidswithguns.py
+++ kidswithguns.py
@@ -1,4 +1,38 @@
-import pandas as pd 
+import pandas as pd
 
 teens_w_guns = pd.read_csv("/Volumes/MyPassportforMac/Code-Louisville/tracking_Louisville_crime/LMPD_JuvenilesWithGunsClean_Jan23-Dec23.csv", index_col = 0)
-pd.DataFrame(teens_w_guns)
+print(teens_w_guns)
+
+
+teens_w_guns.columns 
+
+teens_w_guns.Gender.value_counts()
+
+
+teens_w_guns.Age.value_counts()
+
+teens_w_guns.Gender.value_counts()
+
+teens_w_guns.Race.value_counts()
+
+teens_w_guns.sum
+
+388 + 98 + 2 
+
+
+teens_w_guns.Ethnicity.value_counts()
+
+461 + 14 + 14
+
+teens_w_guns[teens_w_guns.Ethnicity.isnull()]
+
+
+teens_w_guns.Age.pop
+
+teens_w_guns.Age.value_counts()
+
+teens_w_guns.Ethnicity.value_counts()
+
+teens_w_guns.shape
+
+teens_w_guns.Charges Names.value_counts
