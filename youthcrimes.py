82a4457b9860a21559f236598cec1fe00043ca46
marresaburke <marresa.burke@gmail.com>
2024-08-04 18:53:03 -0400

    loaded wrong data sheet in python file

--- youthcrimes.py
+++ youthcrimes.py
@@ -3,19 +3,32 @@ import numpy as np
 from datetime import datetime
 
 #Loading in Data 
-teen_data = pd.read_csv("/Volumes/MyPassportforMac/Code-Louisville/tracking_Louisville_crime/LMPD_JuvenilesWithGunsClean_Jan23-Dec23.csv")
+teen_data = pd.read_csv('LMPD_CleanedData_YouthCrimeFull_2023-2024.csv')
 teen_data.set_index('Date')
 
 guns_demographics = pd.read_csv('LMPD_JuvenilesWithGunsClean_Jan23-Dec23.csv')
 guns_demographics.set_index('Date')
 guns_demographics = guns_demographics.drop(columns=['Agency Name']) #Remove LMPD from data 
-
+guns_demographics
 #Cleaning data by dates 
 
-teen_date_values = pd.DatetimeIndex(teen_data["Date"]) #convert Date column
+month_dict = {1:'January', 
+              2:'February', 
+              3: 'March',
+              4: 'April',
+              5: 'May', 
+              6: 'June',
+              7: 'July',
+              8: 'August',
+              9: 'September', 
+              10: 'October',
+              11: 'November',
+              12: 'December'}
 
+teen_date_values = pd.DatetimeIndex(teen_data["Date"]) #convert Date column
 teen_data['Year'] = teen_date_values.year #seperate the year
 teen_data['Month'] = teen_date_values.month #seperate the month 
+teen_data['Month'] = teen_data['Month'].map(month_dict)
 
 
 handgun = ['POSS HANDGUN BY MINOR 1ST OFFENSE 527.100 52206 520','RECEIVING STOLEN PROPERTY (FIREARM) 514.110 28020 280', 'CARRYING A CONCEALED DEADLY WEAPON 527.020 01501 520','POSS HANDGUN BY MINOR 2ND OR > 527.100 52205 520', 'TBUT OR DISP FIREARM 514.030 23100 23H', 'POSSESSION OF HANDGUN BY CONVICTED FELON 527.040 52197 520','UNLAWFUL POSSESSION OF WEAPON ON SCHOOL PROPERTY 527.070 52204 520', 'UNLAWFULLY PROV/PERMIT MINOR TO POSSESS HANDGUN 527.110 52203 520','USING RESTRICTED AMMO DURING A FELONY (NO SHOTS) 527.080(2)(A) 02549 520', 'POSSESSION OF DEFACED FIREARM 527.050 01504 520','POSSESSION OF FIREARM BY CONVICTED FELON 527.040 52196 520']
