7d95d2db2e60f5c96f23cab76002888af82e10a1
marresaburke <marresa.burke@gmail.com>
2024-08-03 20:28:11 -0400

    stopped at needed lambda for keyword search in teen_crime data frame

--- youthcrimes.py
+++ youthcrimes.py
@@ -1,8 +1,15 @@
 import pandas as pd
+import numpy as np
 
-teens_w_guns = pd.read_csv("/Volumes/MyPassportforMac/Code-Louisville/tracking_Louisville_crime/LMPD_JuvenilesWithGunsClean_Jan23-Dec23.csv")
+#Loading in Data 
+teen_data = pd.read_csv("/Volumes/MyPassportforMac/Code-Louisville/tracking_Louisville_crime/LMPD_JuvenilesWithGunsClean_Jan23-Dec23.csv")
+
 guns_demographics = pd.read_csv('LMPD_JuvenilesWithGunsClean_Jan23-Dec23.csv', index_col=0)
 
+#Drop redundant or unneed information 
+guns_demographics.drop(columns=['Agency Name'])
+
+teen_data.drop(columns=['Charges Sequence #'])
 
 
 
