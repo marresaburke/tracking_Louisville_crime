cfb152bed3eb24cb5b64a726744aa0decafce6d9
marresaburke <marresa.burke@gmail.com>
2024-08-05 19:34:58 -0400

    Next step
    Total teens crimes data frame broken down by ages and months VS gun possession related crimes broken down by age and months  THEN merge those together on Age

--- youthcrimes.py
+++ youthcrimes.py
@@ -24,8 +24,13 @@ guns_demographics['Year'] = gun_date_values.year #separate the year
 guns_demographics['Month'] = gun_date_values.month #seperate the month 
 
 guns_demographics['Month'] = guns_demographics['Month'].map(month_dict)
-guns_demographics
 
+
+#sorted by date range
+dates_teen_full_df = teen_data[(teen_data['Date'] > '2023-01-01') & (teen_data['Date'] < '2023-12-31')]
+dates_gun_full_df = guns_demographics[(guns_demographics['Date'] > '2023-01-01') & (guns_demographics['Date'] < '2023-12-31')]
+
+#filter full teen data with charges only included in gun charges
 handgun = ['POSS HANDGUN BY MINOR 1ST OFFENSE 527.100 52206 520','RECEIVING STOLEN PROPERTY (FIREARM) 514.110 28020 280', 'CARRYING A CONCEALED DEADLY WEAPON 527.020 01501 520','POSS HANDGUN BY MINOR 2ND OR > 527.100 52205 520', 'TBUT OR DISP FIREARM 514.030 23100 23H', 'POSSESSION OF HANDGUN BY CONVICTED FELON 527.040 52197 520','UNLAWFUL POSSESSION OF WEAPON ON SCHOOL PROPERTY 527.070 52204 520', 'UNLAWFULLY PROV/PERMIT MINOR TO POSSESS HANDGUN 527.110 52203 520','USING RESTRICTED AMMO DURING A FELONY (NO SHOTS) 527.080(2)(A) 02549 520', 'POSSESSION OF DEFACED FIREARM 527.050 01504 520','POSSESSION OF FIREARM BY CONVICTED FELON 527.040 52196 520']
 gun_filter =teen_data[teen_data['Charges'].isin(handgun)]
 
